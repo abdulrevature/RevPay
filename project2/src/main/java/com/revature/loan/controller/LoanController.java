54e6af3202c1a2a051f21a9205590d3b2ec64a6e
Caleb Mcolin <Caleb.mcolin@gmail.com>
2023-10-13 13:36:09 -0700

    added controller, service and DAO w/functions

--- project2/src/main/java/com/revature/loan/controller/LoanController.java
+++ project2/src/main/java/com/revature/loan/controller/LoanController.java
@@ -1,8 +1,12 @@
 package com.revature.loan.controller;
 
+import com.revature.loan.model.Loan;
 import com.revature.loan.service.LoanService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin()
@@ -15,4 +19,31 @@ public class LoanController {
     public LoanController(LoanService loanService) {
         this.loanService = loanService;
     }
+
+    @GetMapping
+    public ResponseEntity<List<Loan>> getAllLoans() {
+        return ResponseEntity.ok().body(loanService.getAllLoans());
+    }
+
+    @GetMapping("/{loanId}")
+    public ResponseEntity<Object> getLoanById(@PathVariable("loanId") int loanId) {
+        try {
+            return ResponseEntity.ok().body(loanService.getLoanById(loanId));
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PatchMapping("/{loanId}/approval")
+    public ResponseEntity<Object> updateLoanIsApprovedById(@PathVariable("loanId") int loanId, @RequestBody boolean isApproved) {
+        try {
+            return ResponseEntity.accepted().body(loanService.updateLoanIsApprovedById(loanId, isApproved));
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+
 }
--- project2/src/main/java/com/revature/loan/service/LoanService.java
+++ project2/src/main/java/com/revature/loan/service/LoanService.java
@@ -1,8 +1,12 @@
 package com.revature.loan.service;
 
+import com.revature.loan.model.Loan;
 import com.revature.loan.dao.LoanDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LoanService {
@@ -13,4 +17,35 @@ public class LoanService {
     public LoanService(LoanDAO loanDAO) {
         this.loanDAO = loanDAO;
     }
+
+    public List<Loan> getAllLoans() {
+        return loanDAO.findAll();
+    }
+
+    public Loan getLoanById(int loanId) {
+        if (loanId <= 0) {
+            throw new IllegalArgumentException("Loans with an id of 0 or less cannot exist.");
+        }
+        Optional<Loan> loan = loanDAO.findById(loanId);
+        if (loan.isEmpty()) {
+            throw new IllegalArgumentException("Loans with an id of " + loanId + " does not exist.");
+        }
+        return loan.get();
+    }
+
+    public Loan updateLoanIsApprovedById(int loanId, boolean isApproved) {
+        if (loanId <= 0) {
+            throw new IllegalArgumentException("Loans with an id of 0 or less cannot exist.");
+        }
+        Optional<Loan> oldLoan = loanDAO.findById(loanId);
+        if (oldLoan.isEmpty()) {
+            throw new IllegalArgumentException("Loan with ID of " + loanId + " does not exist.");
+        } else {
+            Loan newLoan = oldLoan.get();
+            newLoan.setLoanApproval(isApproved);
+            newLoan.setLoanReviewed(true);
+            return loanDAO.save(newLoan); //perform the update
+        }
+    }
+
 }
