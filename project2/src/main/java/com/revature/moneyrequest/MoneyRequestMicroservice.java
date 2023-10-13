5b0a737f6f21ce5be1f40cdc8169367a567c94b8
brytat <88180861+brytat@users.noreply.github.com>
2023-10-12 19:00:56 -0700

    Delete project2/src/main/java/com/revature/moneyrequest directory

--- project2/src/main/java/com/revature/moneyrequest/MoneyRequestMicroservice.java
+++
@@ -1,9 +0,0 @@
-package com.revature.moneyRequest;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class MoneyRequestMicroservice {
-    public static void main(String[] args) { SpringApplication.run(MoneyRequestMicroservice.class, args); }
-}
