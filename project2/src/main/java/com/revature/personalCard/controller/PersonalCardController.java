e1845060709b926a36c35db59565447ad5059447
gapcguy <57337947+gapcguy@users.noreply.github.com>
2023-10-13 18:23:18 -0400

    Creates a blank personal card service and personal card controller

--- project2/src/main/java/com/revature/personalCard/controller/PersonalCardController.java
+++ project2/src/main/java/com/revature/personalCard/controller/PersonalCardController.java
@@ -1,4 +1,29 @@
 package com.revature.personalCard.controller;
 
+import com.revature.personalCard.model.PersonalCard;
+import com.revature.personalCard.service.PersonalCardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class PersonalCardController {
+
+    private PersonalCardService personalCardService;
+
+    @Autowired
+    public PersonalCardController(PersonalCardService pcService) {
+        this.personalCardService = pcService;
+    }
+
+
+
+    @PostMapping("/newCard")
+    public ResponseEntity<PersonalCard> createCard(@RequestBody PersonalCard personalCard) {
+        PersonalCard pc = PersonalCardService.createNewCard("Debit", )
+    }
 }
