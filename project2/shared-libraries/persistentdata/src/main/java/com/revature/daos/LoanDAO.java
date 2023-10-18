09d6b460909c8d56eb907fdd7dfd182bf089837c
Caleb Mcolin <Caleb.mcolin@gmail.com>
2023-10-17 17:25:42 -0700

    added LoanDAO

---
+++ project2/shared-libraries/persistentdata/src/main/java/com/revature/daos/LoanDAO.java
@@ -0,0 +1,10 @@
+package com.revature.daos;
+
+import com.revature.models.BusinessCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoanDAO extends JpaRepository<BusinessCard, Integer> {
+
+}
