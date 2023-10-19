0080bd68f2e0da2bd20c6b965e4db933a020b3b8
gapcguy <57337947+gapcguy@users.noreply.github.com>
2023-10-18 22:25:20 -0400

    Rolled email controller into the microservice main function

---
+++ project2/mailer/src/main/java/com/revature/Mailer.java
@@ -0,0 +1,28 @@
+package com.revature;
+
+import com.revature.entity.EmailDetails;
+import com.revature.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.cloud.function.context.FunctionalSpringApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@SpringBootConfiguration
+public class Mailer {
+    public static void main(String[] args) {
+        FunctionalSpringApplication.run(Mailer.class, args);
+
+    }
+
+    private EmailService emailService;
+
+    @Autowired
+    public String send(EmailService emailService, @RequestBody EmailDetails details) {
+        this.emailService = emailService;
+        String status = emailService.sendMail(details);
+
+        return status;
+    }
+
+}
