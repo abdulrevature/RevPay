a7dda2382e271e3a18dc2df6268d4d7f458fb0b0
tcurtis526 <tac526@gmail.com>
2023-10-18 22:07:03 -0400

    bleh changes
    It doesn't really matter but copy pasting this sharedlibraries around will make all our lives easier probably

--- project2/user/createuser/src/main/java/com/revature/sharedLibraries/services/BusinessService.java
+++ project2/user/createuser/src/main/java/com/revature/sharedLibraries/services/BusinessService.java
@@ -1,7 +1,28 @@
 package com.revature.sharedLibraries.services;
 
+import com.revature.sharedLibraries.daos.BusinessCardDAO;
+import com.revature.sharedLibraries.daos.BusinessDAO;
+import com.revature.sharedLibraries.daos.PaymentDAO;
+import com.revature.sharedLibraries.models.Business;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 public class BusinessService {
+    @Autowired
+    private BusinessDAO businessDAO;
+
+    public Business save(Business b) {
+        return businessDAO.save(b);
+    }
+
+    public Business getById(Long id) {
+        return businessDAO.getReferenceById(id);
+    }
+
+    public List<Business> getAll() {
+        return businessDAO.findAll();
+    }
 }
--- project2/user/createuser/src/main/java/com/revature/sharedLibraries/services/MoneyRequestService.java
+++ project2/user/createuser/src/main/java/com/revature/sharedLibraries/services/MoneyRequestService.java
@@ -1,4 +1,12 @@
 package com.revature.sharedLibraries.services;
 
+
+import com.revature.sharedLibraries.models.MoneyRequest;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MoneyRequestService {
+
 }
+
+
