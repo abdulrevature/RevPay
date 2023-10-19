dbfed09e0af4545f8a35945f27690b4e3b2a9966
eogron <67346208+eogron@users.noreply.github.com>
2023-10-19 15:02:55 -0700

    small changes

--- TransactionWithdrawal/src/main/java/com/revature/SpringWithLambda/daos/BusinessDAO.java
+++
@@ -1,9 +0,0 @@
-package com.revature.SpringWithLambda.daos;
-
-import com.revature.SpringWithLambda.models.Business;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BusinessDAO extends JpaRepository<Business, Long> {
-}
--- ViewStatementUser/src/main/java/com/revature/SpringWithLambda/daos/TransactionDAO.java
+++
@@ -1,5 +0,0 @@
-package com.revature.SpringWithLambda.daos;
-
-public class TransactionDAO {
-
-}
