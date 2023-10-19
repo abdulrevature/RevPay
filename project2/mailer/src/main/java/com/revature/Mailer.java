5bb79cc56b3f3853c58ef1cd882a0a274178e40e
gapcguy <57337947+gapcguy@users.noreply.github.com>
2023-10-18 22:37:17 -0400

    Mailer now scans the service class for EmailService

--- project2/mailer/src/main/java/com/revature/Mailer.java
+++ project2/mailer/src/main/java/com/revature/Mailer.java
@@ -5,13 +5,14 @@ import com.revature.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.cloud.function.context.FunctionalSpringApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @SpringBootConfiguration
+@ComponentScan("com.revature.service")
 public class Mailer {
     public static void main(String[] args) {
         FunctionalSpringApplication.run(Mailer.class, args);
-
     }
 
     private EmailService emailService;
